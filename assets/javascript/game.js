fba83cfe2f6a3d1e0b4a56969c699f597cb0aef8
Eladio Montenegro <eladio.montenegro@gmail.com>
2019-07-08 00:42:59 -0500

    added the boxes fot the wins, losses, total score, and found some cool imnages for the crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,12 @@
 //variables
-
+var wins = 0;
+var loses = 0;
+var counter = 0;
+var targetNumber;
 
 
 //crystal image arrays
-
+var images = ["assets/images/crystalblue.jpg","assets/images/crystalpurple.jpg", "assets/images/crystalgreen.jpg", "assets/images/crystalred.jpg"]
 
 
 //create random numbers and show them 
